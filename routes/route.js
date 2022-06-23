de19d01692e68f5d4ab8f49847eefc0769e899b2
AnugyaBansal <bansalanugya66@gmail.com>
2022-06-23 23:26:52 +0530

    Project1

--- routes/route.js
+++ routes/route.js
@@ -8,7 +8,7 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/BASE_URL/authors", AuthorController.createAuthor)
+router.post("/authors", AuthorController.createAuthor)
 router.post("/blogs", BlogController.createBlog)
 router.get('/blogs', BlogController.getAllBlogs)
 router.put("/blogs/:blogId", BlogController.updateBlog)
